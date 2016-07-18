b37218a1393623b3eb886e8a7e7c17db5747303d
Erikang20 <erikang20@gmail.com>
2016-07-18 16:27:08 -0500

    add routes

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require( 'express' );
+var router = express.Router();
+var knex = require( '../db/knex' );
+var methodOverride = require( 'method-override' );
+var bodyParser = require( 'body-parser' );
+
+
+
+router.get( '/', function( req, res, next ) {
+	knex( 'books' ).select().then( function( result, err ) {
+		res.render( 'index' )
+
+	} );
+} );
+
+module.exports = router;
