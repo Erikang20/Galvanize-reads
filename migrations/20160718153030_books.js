7e9e14893a934002756c2f697cd4f1f5d206913d
Erikang20 <erikang20@gmail.com>
2016-07-18 16:13:04 -0500

    add public folder

--- migrations/20160718153030_books.js
+++ migrations/20160718153030_books.js
@@ -3,6 +3,7 @@ exports.up = function( knex, Promise ) {
 	return knex.schema.createTable( 'books', function( table ) {
 		table.increments( 'id' ).primary();
 		table.string( 'title' );
+		table.integer( 'author_id' );
 		// table.integer( 'authorId' ).unsigned().index().references( 'id' ).inTable( 'authors' ).onDelete( 'CASCADE' );
 		table.string( 'country' );
 		table.string( 'img' ).defaultTo( '' );
