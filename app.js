6b2bc9a483ba42919029ca7f26f6e0dacb53d23c
Erikang20 <erikang20@gmail.com>
2016-07-18 10:48:38 -0500

    add data

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var app = angular.module( "library", [ 'ngAnimate' ] );
 
-app.controller( 'books', function( $scope ) {
-	$scope.books = {};
-} )
+app.controller( 'control', function( $scope ) {
+	$scope.library = {};
+
+	$scope.books = [
+		{
+			title: 'Harry Potter and the Prisoner of Azkaban',
+			author: 'J. K. Rowling',
+			img: 'http://t3.gstatic.com/images?q=tbn:ANd9GcQpMUwSXKMjy_ftHrMtOAWMFiAZNaIHuRZK8t0m5DolqCy6MzI2',
+			genre: 'fantasy',
+			description: 'Harry Potter and the Prisoner of Azkaban is the third novel in the Harry Potter series, written by J. K. Rowling. The book follows Harry Potter, a young wizard, in his third year at Hogwarts School of Witchcraft and Wizardry.',
+			comments: 'J. K. Rowling has sidestepped the usual series-writer trap of sticking so closely to a successful formula that each book is just more of the same.'
+
+      },
+		{
+			title: 'Les Misérables',
+			author: 'Victor Hugo',
+			img: 'http://t2.gstatic.com/images?q=tbn:ANd9GcS8cnJAWyJjyDy3WBZ8of0AdmVTsLdGLN6WoKqTf-u4WTQ-Vfqe',
+			genre: 'Historical drama',
+			description: 'Les Misérables is a French historical novel by Victor Hugo, first published in 1862, that is considered one of the greatest novels of the 19th century. In the English-speaking world, the novel is usually referred to by its original French title.',
+			comments: ''
+    }
+
+
+];
+
+} );
