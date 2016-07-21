9c4aaca0e560c64a3c95fe6bd7db526aa792fb33
Erikang20 <erikang20@gmail.com>
2016-07-21 10:51:53 -0500

    setting http get

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,9 @@
-var knex = require( 'knex' );
-
-
-function getBooks() {
-	return knex( 'books' )
-		.select()
-		.innerJoin( 'book_author', 'books.id', 'book_id' )
-		.innerJoin( 'authors', 'authors.id', 'author_id' )
-}
+// var knex = require( 'knex' );
+//
+//
+// function getBooks() {
+// 	return knex( 'books' )
+// 		.select()
+// 		.innerJoin( 'book_author', 'books.id', 'book_id' )
+// 		.innerJoin( 'authors', 'authors.id', 'author_id' )
+// }
--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ var app = angular.module( "library", [ 'ngAnimate', 'ngResource' ] );
 
 app.controller( 'control', function ( $scope, $http ) {
 	$scope.library = {};
+	// $scope.views.library = [];
 	$scope.book = [];
 	$scope.books = [];
 	$scope.author = [];
@@ -71,11 +72,60 @@ app.controller( 'control', function ( $scope, $http ) {
 		console.log( "new authors here yo!" )
 	}
 
-	// $http.get( 'books', {
-	// 	params: {
-	// 		book_id: book.id
+
+
+	$http( {
+		method: "GET",
+		url: '/',
+		// params: {
+		// 	book_id: book.id,
+		// 	book_title: book.title
+		// }
+	} ).then( function mySucces( response ) {
+			// var books = books;
+			// var books = {
+			// 	title: book.title,
+			// 	author: book.author,
+			// 	img: book.img,
+			// 	genre: book.genre,
+			// 	description: book.description,
+			// 	comments: book.comments
+			// }
+			console.log( 'succes' );
+		},
+		function err( response ) {
+			console.log( "Error" );
+		} );
+	// ================================
+	// var data = book;
+	// var data = {
+	// 	// book_id: data.id,
+	// 	title: data.title,
+	// 	author: data.author,
+	// 	img: data.img,
+	// 	genre: data.genre,
+	// 	description: data.description,
+	// 	comments: data.comments
+	// }
+	//
+	// var config = {
+	// 	params: data,
+	// 	headers: {
+	// 		'Accept': '/'
 	// 	}
-	// } )
+	// };
+	// ================================
+
+	$http.get( '/' ).then( function mySucces( response ) {
+		// $scope.books = books;
+		console.log( "getting http" );
+	} )
+
+	$http.post( '/' ).then( function mySucces( response ) {
+		// $scope.books = books;
+		console.log( "posting http!" );
+	} );
+
 
 
 } );
