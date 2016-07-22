e8b7bb0bdba0527fb03c7cd2bc2879d9ce57e002
Erikang20 <erikang20@gmail.com>
2016-07-21 19:21:40 -0500

    home page working

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ var app = angular.module( "myApp", [ 'ngRoute' ] );
 
 app.config( function ( $routeProvider ) {
 	$routeProvider.when( '/', {
-		templateUrl: 'partials/header.html',
+		templateUrl: 'partials/home.html',
 		controller: 'headerController'
 	} )
 } )
